464e9508dab396192600ecf107281f2c7db993de
Allan Max <allan@dottcon.com>
2021-09-05 03:25:00 -0300

    2021.09.05 - Allan Max

---
+++ maxImagem.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * MaxImagem - Compilador de imagens PHP.
+ * PHP Version 7.4.9
+ *
+ * @author      Allan Max <allan@dottcon.com>
+ * @version     1.0.202109
+**/
+
+namespace Fornecedor\MaxImagem;
+
+class MaxImagem
+{
+    /**
+     * Caminho para o diretorio base de armazenamento dos arquivos de imagens.
+     * Pode ser alterado caso queira especificar o caminho para a pasta padrão
+     * onde as imagens ficaram.
+    **/
+    const DIR_IMAGENS = null;
+
+    public function MaxImagem() {
+        //
+    }
+}
